337c937b7313d256c5657abf75f5b82b24e24b29
gideon-steinberg <gideon_steinberg@hotmail.com>
2014-08-16 09:19:55 +1200

    buy and sell stocks in the game

--- public/stocks/game.js
+++ public/stocks/game.js
@@ -1,8 +1,31 @@
 $(document).ready(function(){
   i = 1; 
   window.setInterval(function() {
-   $("#price").text(i);
-   i = i + 1
+   //$("#price").text(i);
+   //i = i + 1;
   }, 1000);
 
 });
+
+function Buy(company){
+  row = $("#" + company);
+  price = parseInt(row.find("#price").text());
+  held = row.find("#held");
+  money = parseInt($("#money").text());
+  if (money >= price){
+    $("#money").text(money - price);
+    held.text(parseInt(held.text()) + 1);
+  }
+}
+
+function Sell(company){
+  row = $("#" + company);
+  price = parseInt(row.find("#price").text());
+  held = row.find("#held");
+  heldAmount = parseInt(held.text());
+  money = parseInt($("#money").text());
+  if (heldAmount > 0){
+    $("#money").text(money + price);
+    held.text(heldAmount - 1);
+  }
+}
