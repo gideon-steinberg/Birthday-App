1aec5dd4aff2ef3d2010f5e214de855972de880d
gideon-steinberg <gideon_steinberg@hotmail.com>
2014-08-16 15:00:32 +1200

    indentation!

--- public/stocks/game.js
+++ public/stocks/game.js
@@ -1,29 +1,28 @@
 $(document).ready(function(){
   window.setInterval(function() {
-   company_number = parseInt($("#company_number").attr("value"));
-   for (i = 0; i < company_number; i++){
-     row = $("#" + i);
-     ran = Math.floor(Math.random()*100);
-     tendency = row.find("#tendency");
-     tendencyVal = parseFloat(tendency.text());
-     price = row.find("#price");
-     if (ran < (tendencyVal * 100)){
-       price.text(parseInt(price.text()) + 1);
-     } else if ( parseInt(price.text()) > 1){
-       price.text(parseInt(price.text()) - 1);
-     }
-     ran = (Math.floor(Math.random()*11) - 5) / 100;
-     newTendency = tendencyVal + ran;
-     if (newTendency < 0.05){
-       newTendency = 0.05;
-     } else if (newTendency > 0.95) {
-       newTendency = 0.95;
-     }
-     newTendency = ("" + newTendency).substring(0,4);
-     tendency.text(newTendency);
-   }  
+    company_number = parseInt($("#company_number").attr("value"));
+    for (i = 0; i < company_number; i++){
+      row = $("#" + i);
+      ran = Math.floor(Math.random()*100);
+      tendency = row.find("#tendency");
+      tendencyVal = parseFloat(tendency.text());
+      price = row.find("#price");
+      if (ran < (tendencyVal * 100)){
+        price.text(parseInt(price.text()) + 1);
+      } else if ( parseInt(price.text()) > 1){
+        price.text(parseInt(price.text()) - 1);
+      }
+      ran = (Math.floor(Math.random()*11) - 5) / 100;
+      newTendency = tendencyVal + ran;
+      if (newTendency < 0.05){
+        newTendency = 0.05;
+      } else if (newTendency > 0.95) {
+        newTendency = 0.95;
+      }
+      newTendency = ("" + newTendency).substring(0,4);
+      tendency.text(newTendency);
+    }  
   }, 1000);
-
 });
 
 function Buy(company){
