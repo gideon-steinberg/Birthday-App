cbf1276dd12cc16e504cfdb6405f148162726037
gideon-steinberg <gideon_steinberg@hotmail.com>
2014-08-19 20:08:34 +1200

    upadte jquery selector in button.js

--- public/games/button.js
+++ public/games/button.js
@@ -23,7 +23,7 @@
     }
 
     $(document).ready(function(){
-        $('.game').click(function () {
+        $('button.game').click(function () {
             moveButton(this);
             increaseScore();
         });
